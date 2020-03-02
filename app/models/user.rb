73f1ff8b770d6864640b5b01646c47a77dc71882
Akinade Tunmise <akinadetunmise8@gmail.com>
2020-03-02 09:01:39 +0100

    Changes made

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ApplicationRecord
 
     # Sends activation email
     def send_activation_email
-        UserMailer.account_activation(self).deliver_now
+        # UserMailer.account_activation(self).deliver_now
     end 
 
     # Sets the password reset attributes
